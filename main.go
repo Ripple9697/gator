8b5c9bd2a49b922c2c05b96ce9464640e95ad5a1
GerryO <gerald0@keemail.me>
2026-07-13 00:14:17 -0500

    robo

--- main.go
+++ main.go
@@ -3,25 +3,33 @@ package main
 import (
 	"fmt"
 	"log"
-
+	"os"
 	"github.com/Ripple9697/gator/internal/config"
 )
 
 func main() {
+	fmt.Println("started")
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
-
-	err = cfg.SetUser("gerald")
-	if err != nil {
-		log.Fatalf("couldn't set current user: %v", err)
-	}
-
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
-	}
 	fmt.Printf("Read config again: %+v\n", cfg)
+
+
+	stg := state{cfg: &cfg}
+	arguments :=  os.Args 
+	if len(arguments) < 2 {
+		log.Fatalf("Required command name")
+	}
+
+	cmds := commands{handlers: make(map[string]func(*state, command) error)}
+
+	cmds.register("login",handlerLogin)
+
+	cmd := command{arguments[1],arguments[2:]}
+
+	err = cmds.run(&stg,cmd)
+	if err != nil {
+		log.Fatalf("cannot run? %+v\n",err)
+	}
 }
